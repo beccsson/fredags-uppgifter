8a8704b85f70cbf7e8fb36dbe0a1c59fa3285f26
unknown <rebecca.anastasiou@gmail.com>
2017-09-01 13:23:12 +0200

    uppgift b

---
+++ Programb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in en mening");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+
+            Array.Sort(words, (x, y) => x.Length.CompareTo(y.Length));
+
+            foreach (string word in words)
+
+            {
+                Console.WriteLine(word);
+            }
+            Array.Reverse(words);
+            foreach (string word in words)
+
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadKey();
+        }
+    }
+}
