f5175893f4bf0f99fece03d29715b8fadf8eac5e
unknown <rebecca.anastasiou@gmail.com>
2017-09-01 13:14:46 +0200

    Fråga A klar

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace u01091701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en mening.");
+            String s = Console.ReadLine();
+
+
+            String[] antal = s.Split(' ');
+            Console.WriteLine(antal.Length);
+
+
+            foreach (String word in antal)
+            {
+                int numberOfLetters = word.Length;
+                Console.WriteLine(word + " (" + numberOfLetters + ")");
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
